3b16aac06d9ff5286706aea83048927e30e9f4fb
Kristian Hov Bjerke <kristianhb87@gmail.com>
2018-01-15 16:28:58 +0000

    First commit

---
+++ tutorial/accounts/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    numbers = [1,2,3,4,5]
+    name = 'Kristian Hov Bjerke'
+
+    args = {'myName': name, 'numbers': numbers}
+    return render (request, 'accounts/home.html', args)
