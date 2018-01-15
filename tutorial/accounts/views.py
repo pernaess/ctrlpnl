b676c8e487faf988f1fc5b19c35f8c1d9b67c5a7
kristian <kristianhb87@gmail.com>
2018-01-15 20:28:59 +0100

    Created registration form

--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from accounts.forms import RegistrationForm
 
-# Create your views here.
+
 def home(request):
     numbers = [1,2,3,4,5]
     name = 'Kristian Hov Bjerke'
 
     args = {'myName': name, 'numbers': numbers}
-    return render (request, 'accounts/home.html', args)
+    return render(request, 'accounts/home.html', args)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account/')
+    else:
+        form = RegistrationForm()
+        args = {'form': form}
+
+        return render(request, 'accounts/reg_form.html', args)
