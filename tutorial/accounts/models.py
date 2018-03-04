5058b8c098e57ccd9d92eafdfad32e38e519792f
kristian <kristianhb87@gmail.com>
2018-03-04 16:39:03 +0100

    Fixed active class menu and icons(not finished)

--- tutorial/accounts/models.py
+++ tutorial/accounts/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 
-class UserProfileManager(models.Manager):
-    def get_queryset(self):
-        return super(UserProfileManager, self).get_queryset().filter(city='Horten')
+# class UserProfileManager(models.Manager):
+#    def get_queryset(self):
+#        return super(UserProfileManager, self).get_queryset().filter(city='Horten')
 
 
 class UserProfile(models.Model):
@@ -18,8 +18,8 @@ class UserProfile(models.Model):
     phone = models.IntegerField(default=0)
     image = models.ImageField(upload_to='profile_image', blank=True)
 
-    Horten = UserProfileManager()
-    objects = models.Manager()
+#   Horten = UserProfileManager()
+#   objects = models.Manager()
 
     def __str__(self):
         return self.user.username
